be6bcf971584500a20aa0611b3fe48adbc7f8fc0
Kapil Singh <singh.kapy@gmail.com>
2017-02-03 12:15:12 +0000

    Finished sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user
-      
+    if @user.save
+      redirect_to @user
     else
       render 'new'
     end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,6 +13,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
-    assert_select "title", full_title()
+    assert_select "title", full_title("Sign up")
   end
 end
