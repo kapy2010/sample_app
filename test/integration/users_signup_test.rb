ab7b19c92f7cca6a1d350fe4f29671683b420440
Kapil Singh <singh.kapy@gmail.com>
2017-02-04 12:41:16 +0530

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,12 +5,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference 'User.count' do
       post signup_path, params: { user: { name: "",
-                                         email: "user@invalid",
-                                         password: "foo",
-                                         password_confirmation: "bar" } }
+                                          email: "user@invalid",
+                                          password: "foo",
+                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation>ul>li', 4
     assert_select 'form[action="/signup"]'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count' do
+      post signup_path, params: { user: { name: "Example User",
+                                          email: "user@example.com",
+                                          password: "password",
+                                          password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
